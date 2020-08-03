a2f2a0808f86fb1cb39c0c41170cd7d3dcb4e9b2
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-08-03 20:55:37 +0300

    Get, Update & Delete Methods With Firebase

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employee: string;
+  employee: any;
   employeeName: string;
   employeeAge: number;
   employeeAddress: string;
@@ -15,11 +15,26 @@ export class EmployeesComponent implements OnInit {
 
   constructor(public employeeservice: EmployeeService) { }
 
+  ngOnInit(): void {
+    this.employeeservice.get_Allemployee().subscribe(data => {
+      this.employee = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isedit: false,
+          name: e.payload.doc.data()['name'],
+          age: e.payload.doc.data()['age'],
+          address: e.payload.doc.data()['address']
+        };
+      })
+      console.log(this.employee);
+    });
+  }
+
   CreateRecord() {
     let Record = {};
-    Record['Name'] = this.employeeName;
-    Record['Age'] = this.employeeAge;
-    Record['Address'] = this.employeeAddress;
+    Record['name'] = this.employeeName;
+    Record['age'] = this.employeeAge;
+    Record['address'] = this.employeeAddress;
 
     this.employeeservice.create_Newemployee(Record).then(res => {
       this.employeeName = '';
@@ -32,7 +47,24 @@ export class EmployeesComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  EditRecord(Record) {
+    Record.isedit = true;
+    Record.editname = Record.name;
+    Record.editage = Record.age;
+    Record.editaddress = Record.address;
+  }
+
+  Updaterecord(recorddata) {
+    let record = {};
+    record['name'] = recorddata.editname;
+    record['age'] = recorddata.editage;
+    record['address'] = recorddata.editaddress;
+    this.employeeservice.update_employee(recorddata.id, record);
+    recorddata.isedit = false;
+  }
+
+  Deleteemployee(record_id) {
+    this.employeeservice.delete_employee(record_id);
   }
 
 }
